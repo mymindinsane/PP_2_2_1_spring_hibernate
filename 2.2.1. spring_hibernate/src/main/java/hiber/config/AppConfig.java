4685b8188378037212a49d4365496a3c50d25e7f
mymindinsane <arturguletskiy21@gmail.com>
2025-02-15 12:04:01 +0300

    feat: implemented HikariCP connection pool

--- 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
@@ -1,5 +1,6 @@
 package hiber.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -7,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -27,9 +27,9 @@ public class AppConfig {
 
    @Bean
    public DataSource getDataSource() {
-      DriverManagerDataSource dataSource = new DriverManagerDataSource();
+      HikariDataSource dataSource = new HikariDataSource();
       dataSource.setDriverClassName(env.getProperty("db.driver"));
-      dataSource.setUrl(env.getProperty("db.url"));
+      dataSource.setJdbcUrl(env.getProperty("db.url"));
       dataSource.setUsername(env.getProperty("db.username"));
       dataSource.setPassword(env.getProperty("db.password"));
       return dataSource;
